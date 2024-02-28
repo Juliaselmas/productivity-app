e548b04e5a97ec2a2aef140166ccee39c65d27d7
sofia-creates <sofia.ledin2@gmail.com>
2024-02-28 18:02:41 +0100

    toggla synlighet på sortering & filtrering

--- login.js
+++ login.js
@@ -142,6 +142,35 @@ logOutBtn.addEventListener("click", () => {
 
 
 
+//dölj eller visa sorting och filter knappar, flytta till todo sen
+//välja knapparna
+let showFiltersBtnsNodes = document.querySelectorAll(".showFiltersBtn");
+let showSortingBtnsNodes = document.querySelectorAll(".showSortingBtn");
+
+//eventlisteners på knapparna
+showFiltersBtnsNodes.forEach(function(button) {
+    button.addEventListener('click', function() {
+        //välja rutan
+        let parentColumn = this.parentNode;
+        let closestFilterBox = parentColumn.querySelector('.filterBox');
+
+        //toggla klassen .hide när man klickar
+        closestFilterBox.classList.toggle('hide');
+    });
+});
+
+//sortering
+showSortingBtnsNodes.forEach(function(button) {
+    button.addEventListener('click', function() {
+        //välja rutan
+        let parentColumn = this.parentNode;
+        let closestSortingBox = parentColumn.querySelector('.sortingBox');
+
+        //toggla klassen .hide när man klickar
+        closestSortingBox.classList.toggle('hide');
+    });
+});
+
 
 
 

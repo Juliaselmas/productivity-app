9ac10fc69d3073dd7153fe5b432daa0c1b48b647
sofia-creates <sofia.ledin2@gmail.com>
2024-03-01 14:06:46 +0100

    hämtat ner senaste uppdateringar fr main

--- login.js
+++ login.js
@@ -191,14 +191,14 @@ showSortingBtnsNodes.forEach(function(button) {
 //     console.log(currentUserObject);
 //     currentUser = JSON.stringify(currentUserObject); //konverterar tillbaka till en sträng
 
-    
+
 //     localStorage.setItem("currentUser" , currentUser); // uppdaterar currentUser till det nya som har skapats
-    
-    
+
+
 //     //hämta motsvarande user frånusers array och uppdatera den med nya tasks, stoppa sedan tillbaka den i users arrayn
-    
+
 //     let users = JSON.parse(localStorage.getItem ("users")) || []; // hämta tidigare data alternativt skapa en tom array
-    
+
 //     //tittar i arrayn med users och väljer den användaren som legat i arrayn
 //     let thisUserInTheArray = users.find(
 //         (user) => user.username === currentUser.username && user.password === currentUser.password
@@ -207,16 +207,16 @@ showSortingBtnsNodes.forEach(function(button) {
 //         console.log(currentUserObject.tasks);        
 //     //byta ut tasks i användaren i users mot currentusers tasks
 //     thisUserInTheArray.tasks = currentUserObject.tasks;
-       
+    
 //     console.log(thisUserInTheArray);
-        
-        
+    
+    
 //         // let previousUser = users.find(
 //     //     (user) => user.username === currentUser.username && user.password === currentUser.password
 //     //     );
 //     // let updatedUser = currentUser; //uppdaterar så att updatedUser matchar currentUser
 
-    
+
 //     // let index = users.findIndex(
 //     // (user) => user.username === previousUser.username && user.password === previousUser.password
 //     // ); // skapa ett index för var i users arrayen som användaren vi jobbar med ligger
@@ -224,11 +224,11 @@ showSortingBtnsNodes.forEach(function(button) {
 //     // if (index !== -1) {
 //     //     // ersätta previousUser med updatedUser med hjälp av indexet
 //     //     users[index] = updatedUser;
-        
+    
 //         // spara den uppdaterade användaren tillbaka till localStorage
 //     localStorage.setItem("users", JSON.stringify(users));
 //     }
-    
+
 
 //     //hämta tasks[ ] och lägg in dem i currentUser
 
